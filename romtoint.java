fa2a91165053598cdbca982b8cfffed3baed1a10
Kaviyashree M <govianju321@gmail.com>
2026-08-03 00:17:12 +0530

    Add files via upload

---
+++ romtoint.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int romanToInt(String s) {
+        int[] value = new int[256];
+
+        value['I'] = 1;
+        value['V'] = 5;
+        value['X'] = 10;
+        value['L'] = 50;
+        value['C'] = 100;
+        value['D'] = 500;
+        value['M'] = 1000;
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && value[s.charAt(i)] < value[s.charAt(i + 1)]) {
+                result -= value[s.charAt(i)];
+            } else {
+                result += value[s.charAt(i)];
+            }
+        }
+
+        return result;
+    }
+}
